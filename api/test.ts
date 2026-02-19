61d60d58c3322c331ab6d69dfd9189c8fcfaae1f
ekanshsaxena <esaxena9927@gmail.com>
2026-02-19 23:01:31 +0530

    refactor: clean up - separate modules, remove test, restore vercel.json

--- api/test.ts
+++
@@ -1,5 +0,0 @@
-export default function handler(req: any, res: any) {
-  res.setHeader("Content-Type", "text/plain");
-  res.statusCode = 200;
-  res.end("Hello from Vercel! Node version: " + process.version);
-}
